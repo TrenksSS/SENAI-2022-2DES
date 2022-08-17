ede1c140cef7e894d33e4b9f2a76b109e65e7da8
unknown <TrenksSS>
2022-08-17 11:34:33 -0300

    aula 10 front js

---
+++ FE/Aula 10 js/cadastro.js
@@ -0,0 +1,13 @@
+var priNome = document.querySelector("#p-nome").value
+var sobrenome = document.querySelector("#sobrenome").value
+var email = document.querySelector("#email").value
+var senha = document.querySelector("#senha").value
+var confirmaSenha = document.querySelector("#c-senha").value
+var telefone = document.querySelector("#telefone").value
+var cep = document.querySelector("#cep").value
+
+function cadastrar(){
+    if()
+
+
+}
