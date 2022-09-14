50de1052b4ea39e102c60dd3c8e324add4fb848c
unknown <TrenksSS>
2022-09-14 11:04:42 -0300

    ex aula 7

--- BE/Aula 7 be/Ex7/lancamentos_front/index.js
+++ BE/Aula 7 be/Ex7/lancamentos_front/index.js
@@ -1,22 +1,61 @@
+var url = 'http://localhost:3000/lancamentos/lancamento'
+var lancamentos = []
+const allLac = document.querySelector("#allLac")
+const allLac2 = document.querySelector("#allLac2")
 const listaLancamentos = document.querySelector("#lista-lancamento")
 const linhamodelo = document.querySelector(".linhamodelo")
 
-fetch("http://localhost:3000/lancamentos/lancamento")
-    .then(res => { return res.json() })
-    .then(lancamentos => {
-        lancamentos.forEach(lancamento => {
-            let linha = linhamodelo.cloneNode(true);
-            linha.classList.remove("model");
-
-            let colunas = linha.querySelectorAll("td");
-            colunas[0].innerHTML = lancamento.n_lancamento;
-            colunas[1].innerHTML = lancamento.data;
-            colunas[2].innerHTML = lancamento.descricao;
-            colunas[3].innerHTML = "R$ " + lancamento.valor;
-            colunas[4].innerHTML = lancamento.tipo;
-
-            
-            listaLancamentos.appendChild(linha);
-        })
-        
+function loadLanc() {
+    fetch(url)
+    .then(res => {return res.json()})
+    .then(data =>{
+        lancamentos = data
+        preencheTabela()
     })
+}
+
+function preencheTabela() {
+    let indice = 0
+    lancamentos.forEach(e =>{
+        if(e.tipo == 'D'){
+        let linha = document.createElement("tr")
+        let cod = document.createElement("td")
+        let data = document.createElement("td")
+        let desc = document.createElement("td")
+        let valor = document.createElement("td")
+        let tipo = document.createElement("td")
+         cod.innerHTML = e.n_lancamento
+         data.innerHTML = e.data.slice(0,10)
+         desc.innerHTML = e.descricao
+         valor.innerHTML = e.valor
+         tipo.innerHTML = e.tipo
+         linha.appendChild(cod) 
+         linha.appendChild(data)
+         linha.appendChild(desc)
+         linha.appendChild(valor)
+         linha.appendChild(tipo)
+         allLac.appendChild(linha)
+         indice++  
+        }else if(e.tipo == 'C'){
+            let linha = document.createElement("tr")
+            let cod = document.createElement("td")
+            let data = document.createElement("td")
+            let desc = document.createElement("td")
+            let valor = document.createElement("td")
+            let tipo = document.createElement("td")
+             cod.innerHTML = e.n_lancamento
+             data.innerHTML = e.data.slice(0,10)
+             desc.innerHTML = e.descricao
+             valor.innerHTML = e.valor
+             tipo.innerHTML = e.tipo
+             linha.appendChild(cod) 
+             linha.appendChild(data)
+             linha.appendChild(desc)
+             linha.appendChild(valor)
+             linha.appendChild(tipo)
+             allLac2.appendChild(linha)
+             indice++ 
+        }
+
+    })
+}
