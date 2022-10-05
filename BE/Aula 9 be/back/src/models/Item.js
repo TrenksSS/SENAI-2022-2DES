bb87469bbb448e924435ccace857477d6620fd27
unknown <TrenksSS>
2022-10-05 09:04:06 -0300

    aula 9 be

--- BE/Aula 9 be/back/src/models/Item.js
+++ BE/Aula 9 be/back/src/models/Item.js
@@ -1,15 +1,39 @@
-const toReadAll = () => {
-    return "SELECT * FROM itens"
-}
 const toCreate = (model) => {
-    return `INSERT INTO itens VALUES (${model.ni}, '${model.aquisicao}', ${model.valor}, '${model.img}')`
+    return `INSERT INTO itens VALUES (${model.ni},'${model.aquisicao}','${model.denominacao}',${model.valor},'${model.img}')`;
+}
+const toReadAll = () => {
+    return "SELECT * FROM itens";
+}
+const toRead = (model) => {
+    return `SELECT * FROM itens  WHERE ni=${model.ni}`;
+}
+
+const toReadNome = (model) => {
+    return `SELECT * FROM itens  WHERE denominacao like '%${model.denominacao}%'`;
+}
+
+const toReadData = (model) => {
+    return `SELECT * FROM itens  WHERE aquisicao like '%${model.aquisicao}%'`;
+}
+
+const toUpdate = (model) => {
+    return `UPDATE itens SET 
+                aquisicao = '${model.aquisicao}',
+                denominacao = '${model.denominacao}',
+                valor = ${model.valor},
+                img = '${model.img}'
+                WHERE ni = ${model.ni}`;
 }
 const toDelete = (model) => {
-    return `DELETE FROM itens WHERE ni=${model.ni}`
+    return `DELETE FROM itens WHERE ni=${model.ni}`;
 }
 
 module.exports = {
-    listarItens,
-    criarItens,
-    excluirItens
+    toCreate,
+    toReadAll,
+    toRead,
+    toReadNome,
+    toReadData,
+    toUpdate,
+    toDelete
 }
---
+++ BE/Aula 9 be/front/itens.js
@@ -0,0 +1,45 @@
+const uri = 'http://localhost:3000/patrimonio/itens/';
+const corpo = document.querySelector("#corpo");
+var itens = [];
+
+const carregar = () => {
+    const options = { method: 'GET' };
+    fetch(uri, options)
+        .then(res => res.json())
+        .then(res => {
+            itens = res;
+            preencherTela();
+        })
+        .catch(err => console.error(err));
+}
+
+const preencherTela = () => {
+    itens.forEach(e => {
+      console.log(e.valor)
+        let card = document.querySelector(".card").cloneNode(true);
+        card.classList.remove('model');
+        card.querySelector("#ni").innerHTML += e.ni;
+        card.querySelector("#aquisicao").innerHTML += e.aquisicao.slice(0,10);
+        card.querySelector("#denominacao").innerHTML += e.denominacao;
+        card.querySelector("#valor").innerHTML += e.valo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        card.querySelector("#img").src = '../docs/assets/' + e.img;
+        card.querySelector("#del").setAttribute('onclick', `del(${e.ni})`);
+        corpo.appendChild(card);
+
+    });
+}
+
+const del = (ni) => {
+    const options = {
+        method: 'DELETE'
+    };
+    if (confirm("Confirma a exclusão do item " + ni)) {
+        fetch(uri + ni, options)
+            .then(res => res.status)
+            .then(res => {
+                if (res == 204)
+                    window.location.reload();
+            })
+            .catch(err => console.error(err));
+    }
+}
